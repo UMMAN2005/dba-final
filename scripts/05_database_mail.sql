2785bbe0b23ccdce18e6e21f0d418a7053745d2e
UMMAN2005 <ummanmemmedov2005@gmail.com>
2026-04-28 20:03:23 +0400

    Init

--- scripts/05_database_mail.sql
+++ scripts/05_database_mail.sql
@@ -9,13 +9,13 @@ GO
 
 EXECUTE msdb.dbo.sysmail_add_account_sp
     @account_name = 'AlertAccount',
-    @email_address = 'sendinblue.brevo@gmail.com',
+    @email_address = 'EMAIL',
     @display_name = 'SQL Alert System',
-    @mailserver_name = 'smtp-relay.brevo.com',
+    @mailserver_name = 'SERVER',
     @port = 587,
     @enable_ssl = 1,
-    @username = 'sendinblue.brevo@gmail.com',
-    @password = 'xsmtpsib-e5206e95ff33ccf54c8619284d2d248d623c8bf7ebb989336bad4c444e3610db-9yOiajME4chGJSkz'; 
+    @username = 'USERNAME',
+    @password = 'PASSWORD';
 
 EXECUTE msdb.dbo.sysmail_add_profile_sp
     @profile_name = 'AdminProfile';
@@ -26,9 +26,9 @@ EXECUTE msdb.dbo.sysmail_add_profileaccount_sp
     @sequence_number = 1;
 GO
 
-EXEC msdb.dbo.sp_add_operator 
-    @name = N'DBA_Team', 
-    @enabled = 1, 
+EXEC msdb.dbo.sp_add_operator
+    @name = N'DBA_Team',
+    @enabled = 1,
     @email_address = N'admin1@lab.local;admin2@lab.local;admin3@lab.local';
 GO
 
